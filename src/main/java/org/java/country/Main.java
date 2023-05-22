87a05c06313ecf71c0a41f5697375f38ef198682
Luigi213 <reyesgianluigi88@gmail.com>
2023-05-22 13:17:42 +0200

    milestone 1 done

--- src/main/java/org/java/country/Main.java
+++ src/main/java/org/java/country/Main.java
@@ -2,6 +2,13 @@ package org.java.country;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("cioa");
+//		milestone 1 done
+//		SELECT c.name, c.country_id , r.name, c2.name 
+//		FROM countries c 
+//			JOIN regions r 
+//			ON c.region_id = r.region_id 
+//			JOIN continents c2 
+//			ON r.continent_id = c2.continent_id 
+//		ORDER BY c.name ASC;
 	}
 }
